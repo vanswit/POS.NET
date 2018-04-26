b5a03a775d1a893a5dd1c99386b8810b61d189b0
Tom Vanswijgenhoven <Tom.Vanswijgenhoven@hotmail.com>
2018-04-26 13:14:57 +0200

    26/04/2018

---
+++ POS.EF/POSContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using POS.BL;
+
+namespace POS.EF
+{
+    public class POSContext : DbContext, IDisposable
+    {
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<PurchaseLine> PurchaseLines { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+    }
+}
--- POS.Repositories/AddressRepository.cs
+++ POS.Repositories/AddressRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using POS.BL;
+using POS.EF;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,11 @@ using System.Threading.Tasks;
 
 namespace POS.Repositories
 {
-    class AddressRepository
+    public class AddressRepository
     {
+        public bool Save(Address address)
+        {
+            return true;
+        }
     }
 }
